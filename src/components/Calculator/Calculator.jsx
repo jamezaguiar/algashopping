8c9d9ce0bf7f4a77eb3a3cccc75683f4428dee04
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-12-08 21:03:35 -0300

    Calculator component

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { sum, subtract } from '../../store/Calculator/Calculator.actions';
+
+const Calculator = () => {
+  const dispatch = useDispatch();
+  const result = useSelector(state => state.calculator.result);
+  const [a, setA] = useState(0);
+  const [b, setB] = useState(0);
+
+  const handleSum = useCallback(() => {
+    dispatch(sum(a, b));
+  }, [a, b, dispatch]);
+
+  const handleSubtract = useCallback(() => {
+    dispatch(subtract(a, b));
+  }, [a, b, dispatch]);
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="a"
+        value={a}
+        onChange={event => setA(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="b"
+        value={b}
+        onChange={event => setB(event.target.value)}
+      />
+      <button onClick={handleSum}>somar</button>
+      <button onClick={handleSubtract}>subtrair</button>
+      <div>{result}</div>
+    </>
+  );
+};
+
+export default Calculator;
--- src/store/Calculator/Calculator.reducer.js
+++ src/store/Calculator/Calculator.reducer.js
@@ -1,11 +1,17 @@
-const initialState = 0;
+const initialState = { result: 0 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SUM':
-      return action.payload[0] + action.payload[1];
+      return {
+        ...state,
+        result: Number(action.payload[0]) + Number(action.payload[1]),
+      };
     case 'SUBTRACT':
-      return action.payload[0] - action.payload[1];
+      return {
+        ...state,
+        result: Number(action.payload[0]) - Number(action.payload[1]),
+      };
     default:
       return state;
   }
